30da79a343447809272564129f985ef17415454b
sergey <serg.matusevich@gmail.com>
2021-01-07 17:28:38 +0300

    crawler few fixes

--- src/main/java/by/matusevich/Crawler.java
+++ src/main/java/by/matusevich/Crawler.java
@@ -10,11 +10,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Crawler {
+    private static final int URL_LIMIT=10000;
     public static Queue<String> queue = new LinkedList<>();
     public static Set<String> marked = new HashSet<>();
-    //    public static String regex = "http[s]*://(\\w+\\.)*(\\w)";
     public static String regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-    public static String[] terms = new String[]{"java", "regex"};
+    public static String[] terms = new String[]{"Tesla", "Musk"};
     public static Map<String, Integer> termMap = new HashMap<>();
 
     public static void crawl(String root) throws IOException {
@@ -25,7 +25,7 @@ public class Crawler {
             String crawledUrl = queue.poll();
             System.out.println("\n=== Site crawled : " + crawledUrl + " ===");
 
-            if (marked.size() > 100)
+            if (marked.size() > URL_LIMIT)
                 return;
             boolean ok = false;
             URL url = null;
@@ -50,45 +50,12 @@ public class Crawler {
 
             while ((tmp = br.readLine()) != null) {
                 sb.append(tmp);
-
             }
             tmp = sb.toString();
-            System.out.println("tmp" + tmp);
-            Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(tmp);
 
-            for (String term : terms
-            ) {
-                Pattern pattern1 = Pattern.compile(term);
-                Matcher matcher1 = pattern1.matcher(tmp);
-                while (matcher1.find()) {
-                    if (termMap.containsKey(term)) {
-                        termMap.put(term, termMap.get(term) + 1);
-                    } else {
-                        termMap.put(term, 1);
-                    }
-                }
-            }
-            //todo парсить страницу +
-            //todo получить урлы +
-            //todo парсить следующие урлы +
-            //todo счетчик количества урлов +
+            searchTerms(tmp, terms);
+            searchUrls(tmp, regex);
 
-            //todo счетчик глубины(структура данных)
-            //todo поиск слов на странице и счетчик - вроде есть
-            //todo таймаут при загрузке страниц
-            while (matcher.find()) {
-                String w = matcher.group();
-                if ((w.contains(".png")) || w.contains(".js") ||
-                        w.contains(".css") || w.contains(".jpg") || w.contains(".xml") || w.contains(".dtd")) {
-                    continue;
-                }
-                if (!marked.contains(w)) {
-                    marked.add(w);
-                    System.out.println("site added :" + w);
-                    queue.add(w);
-                }
-            }
         }
         if (br != null) {
             br.close();
@@ -96,8 +63,8 @@ public class Crawler {
     }
 
     public static void showResults() {
-        System.out.println("\n\nResults");
-        System.out.println("web sites crawled: " + marked.size() + "\n");
+//        System.out.println("\n\nResults");
+//        System.out.println("web sites crawled: " + marked.size() + "\n");
 
         for (String s : marked) {
             System.out.println("* " + s);
@@ -105,21 +72,48 @@ public class Crawler {
         for (Map.Entry<String, Integer> entry : termMap.entrySet()
         ) {
             System.out.println(entry.getKey() + "\t\t\t" + entry.getValue());
+        }
+    }
 
+    public static void searchUrls(String text, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            String w = matcher.group();
+            if ((w.contains(".png")) || w.contains(".js") ||
+                    w.contains(".css") || w.contains(".jpg") || w.contains(".xml") || w.contains(".dtd")) {
+                continue;
+            }
+            if (!marked.contains(w)) {
+                marked.add(w);
+                System.out.println("site added :" + w);
+                queue.add(w);
+            }
+        }
+    }
+
+    public static void searchTerms(String text, String[] terms) {
+        for (String term : terms
+        ) {
+            Pattern pattern1 = Pattern.compile(term);
+            Matcher matcher1 = pattern1.matcher(text);
+            while (matcher1.find()) {
+                if (termMap.containsKey(term))
+                    termMap.put(term, termMap.get(term) + 1);
+                else
+                    termMap.put(term, 1);
+            }
         }
     }
 
     public static void main(String[] args) {
         try {
-            System.out.println(System.currentTimeMillis());
-
-            crawl("https://www.javatpoint.com/java-regex");
-//            crawl("http://www.quizful.net/");
+//            crawl("https://www.javatpoint.com/java-regex");
+            crawl("https://en.wikipedia.org/wiki/Elon_Musk");
         } catch (IOException e) {
             e.printStackTrace();
         }
         showResults();
-        System.out.println(System.currentTimeMillis());
     }
 
 }
