e6906f22e02ad008eaf4ee2f4838c0fa59bcf3f5
sergey <serg.matusevich@gmail.com>
2021-01-07 20:24:45 +0300

    crawler 20,24

--- src/main/java/by/matusevich/Crawler.java
+++ src/main/java/by/matusevich/Crawler.java
@@ -4,26 +4,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Crawler {
-    private static final int URL_LIMIT=10000;
+    private static final int URL_LIMIT = 10000;
+
     public static Queue<String> queue = new LinkedList<>();
     public static Set<String> marked = new HashSet<>();
+
     public static String regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-    public static String[] terms = new String[]{"Tesla", "Musk"};
+
+    public static String[] terms = new String[]{"Java", "regex", "java", "Regex"};
     public static Map<String, Integer> termMap = new HashMap<>();
 
     public static void crawl(String root) throws IOException {
         queue.add(root);
         BufferedReader br = null;
 
+        int i = 1;
         while (!queue.isEmpty()) {
             String crawledUrl = queue.poll();
-            System.out.println("\n=== Site crawled : " + crawledUrl + " ===");
+            System.out.println("\n=== Site crawled : " + marked.size() +" "+ crawledUrl  + " ===" );
 
             if (marked.size() > URL_LIMIT)
                 return;
@@ -32,8 +37,13 @@ public class Crawler {
             while (!ok) {
                 try {
                     url = new URL(crawledUrl);
+                    url.openConnection().setConnectTimeout(3000);
                     br = new BufferedReader(new InputStreamReader(url.openStream()));
                     ok = true;
+                } catch (SocketTimeoutException e) {
+                    System.out.println("*** SocketTimeoutException URL : " + crawledUrl);
+                    crawledUrl = queue.poll();
+                    ok = false;
                 } catch (MalformedURLException e) {
                     System.out.println("*** Malformed URL : " + crawledUrl);
                     crawledUrl = queue.poll();
@@ -80,12 +90,13 @@ public class Crawler {
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
             String w = matcher.group();
-            if ((w.contains(".png")) || w.contains(".js") ||
+            if ((w.contains(".png")) || w.contains(".js") || w.contains(".jpeg") ||
                     w.contains(".css") || w.contains(".jpg") || w.contains(".xml") || w.contains(".dtd")) {
                 continue;
             }
             if (!marked.contains(w)) {
                 marked.add(w);
+                //todo увеличение счетчика глубины
                 System.out.println("site added :" + w);
                 queue.add(w);
             }
@@ -95,9 +106,9 @@ public class Crawler {
     public static void searchTerms(String text, String[] terms) {
         for (String term : terms
         ) {
-            Pattern pattern1 = Pattern.compile(term);
-            Matcher matcher1 = pattern1.matcher(text);
-            while (matcher1.find()) {
+            Pattern pattern = Pattern.compile(term);
+            Matcher matcher = pattern.matcher(text);
+            while (matcher.find()) {
                 if (termMap.containsKey(term))
                     termMap.put(term, termMap.get(term) + 1);
                 else
@@ -108,12 +119,13 @@ public class Crawler {
 
     public static void main(String[] args) {
         try {
-//            crawl("https://www.javatpoint.com/java-regex");
-            crawl("https://en.wikipedia.org/wiki/Elon_Musk");
+            crawl("https://www.javatpoint.com/java-regex");
+//            crawl("https://en.wikipedia.org/wiki/Elon_Musk");
         } catch (IOException e) {
             e.printStackTrace();
         }
         showResults();
+//        printCsv(termMap);
     }
 
 }
